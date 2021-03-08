2ebcd1c255f000dc7b22dcc6fe10ab2b2a76da47
Maurice Gerber <maurice.gerber2@gmail.com>
2021-03-08 13:19:01 +0100

    Update TensorFlow_prediction.py
    update

--- TensorFlow_prediction.py
+++ TensorFlow_prediction.py
@@ -1,5 +1,6 @@
 import quandl
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 # 03988 = Stock price, Bank Of China, date and nominal price (closing price)
@@ -17,15 +18,26 @@ print(dat1)
 n = dat1.shape[0]
 p = dat1.shape[1]
 # Make data a numpy array / Drop date variable
-#dat1.reset_index(inplace=True)
-#dat1.drop(columns=["Date"], inplace=True)
-dat1 = dat1.values
+dat1.reset_index(inplace=True)
+dat1.drop(columns=["Date"], inplace=True)
+#dat1 = dat1.values
 print(dat1)
 plt.plot(dat1)
-plt.show()
+#plt.show()
+
+# Training and test data
+#train_start = 0
+#train_end = int(np.floor(0.8*n))
+#test_start = train_end
+#test_end = n
+#data_train = dat1[np.arange(train_start, train_end), :]
+#data_test = dat1[np.arange(test_start, test_end), :]
 
 
+dat1.tail(5)
 
+#train = head(dat1,round(0.65*nrow(dat1))) 
+#test = tail(dat1,round(0.25*nrow(dat1)))
 
 
 
